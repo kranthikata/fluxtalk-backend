cfc7d8c0ae6d6f716fc043fb6c27c74a32793a04
kranthikata <kranthikumarkata464@gmail.com>
2024-11-10 14:22:05 +0530

    Updated the frontend link for the origin property in cors object

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const { Server } = require("socket.io");
 const { Socket } = require("dgram");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://fluxtalk-frontend.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
